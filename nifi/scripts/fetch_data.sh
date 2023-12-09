623c7d2ee832aac15eacc59ebda03e9eb07abb78
maciejors <maciej.orslowski@gmail.com>
2023-12-09 17:36:11 +0000

    update fetch_data script

--- nifi/scripts/fetch_data.sh
+++ nifi/scripts/fetch_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 timestamp=$(date +%s)
-filename="./data/stackoverflow/questiondddddds.json"
+filename="./data/stackoverflow/questions-${timestamp}.json"
 url="https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
 
 curl -k --compressed -X GET $url -o $filename
