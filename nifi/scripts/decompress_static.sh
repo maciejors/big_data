fcd4066ca490a60603446fc8694f8537890fb4f9
maciejors <maciej.orslowski@gmail.com>
2023-12-10 14:54:20 +0000

    update templates and decompress script

--- nifi/scripts/decompress_static.sh
+++ nifi/scripts/decompress_static.sh
@@ -2,3 +2,8 @@
 filename="./data/datascience/datascience.stackexchange.com.7z"
 output="./data/datascience/"
 7z e $filename -o$output
+# fix some files
+posts_path="${output}Posts.xml"
+posthistory_path="${output}PostHistory.xml"
+xmllint --format $posts_path --output $posts_path
+xmllint --format $posthistory_path --output $posthistory_path
