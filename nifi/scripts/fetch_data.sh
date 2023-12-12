be75cfd1d8308acfd60b9c7f966d6600ac5cfca7
Szymon-Gut <szymon.gut01@gmail.com>
2023-12-12 17:06:51 +0000

    add parameter to fetch_data.sh

--- nifi/scripts/fetch_data.sh
+++ nifi/scripts/fetch_data.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-timestamp=$(date +%s)
+if [ $# -eq 0 ]; then
+    echo "Please provide a timestamp as an argument."
+    exit 1
+fi
+
+timestamp=$1
+
 filename="./data/stackoverflow/questions-${timestamp}.json"
 url="https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
 
-curl -k --compressed -X GET $url -o $filename
+curl -k --compressed -X GET $url -o $filename#!/bin/bash
+
