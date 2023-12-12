8cf7077dc08a0107964c6e6803b2bde488064e69
maciejors <maciej.orslowski@gmail.com>
2023-12-12 16:05:31 +0000

    update LoadStatic flow and related scripts

--- nifi/scripts/decompress_static.sh
+++ nifi/scripts/decompress_static.sh
@@ -5,5 +5,16 @@ output="./data/datascience/"
 # fix some files
 posts_path="${output}Posts.xml"
 posthistory_path="${output}PostHistory.xml"
+echo "Fixing large XMLs structures..."
 xmllint --format $posts_path --output $posts_path
 xmllint --format $posthistory_path --output $posthistory_path
+echo "Done."
+# split large files
+echo "Splitting large XML files..."
+python3 ./nifi/scripts/split_large_xmls.py
+echo "Done."
+echo "Cleanup..."
+rm ${output}/Posts.xml
+rm ${output}/PostHistory.xml
+echo "Done."
+echo "Exiting script."
---
+++ nifi/scripts/split_large_xmls.py
@@ -0,0 +1,44 @@
+import os
+from xml.etree import ElementTree
+
+
+ds_dir = './data/datascience'
+posts_path = f'{ds_dir}/Posts.xml'
+posthistory_path = f'{ds_dir}/PostHistory.xml'
+
+CHUNK_SIZE = 10000
+
+def split(ctx, main_tag):
+    new_dir = f'{ds_dir}/{main_tag}_split'
+    os.makedirs(new_dir, exist_ok=True)
+    for i, (event, elem) in enumerate(context):
+        if elem.tag == 'row':
+            post_id = elem.attrib['Id']
+            if i % CHUNK_SIZE == 0:
+                # end current file
+                if i != 0:
+                    with open(curr_file_path, 'ba') as f:
+                        f.write(f'</{main_tag}>'.encode())
+                # start new file
+                curr_file_path = f'{new_dir}/{main_tag}-{post_id}.xml'
+                with open(curr_file_path, 'ba') as f:
+                    f.write(b'<?xml version="1.0" encoding="utf-8"?>\n')
+                    f.write(f'<{main_tag}>'.encode())
+            with open(curr_file_path, 'ba') as f:
+                f.write(ElementTree.tostring(elem))
+    # end unfinished file
+    if i % CHUNK_SIZE != 0:
+        with open(curr_file_path, 'ba') as f:
+            f.write(f'</{main_tag}>'.encode())
+    else:
+        # remove empty file
+        os.remove(curr_file_path)
+        
+
+# split Posts.xml
+context = ElementTree.iterparse(posts_path, events=('end', ))
+split(context, 'posts')
+
+# split PostHistory.xml
+context = ElementTree.iterparse(posts_path, events=('end', ))
+split(context, 'posthistory')
