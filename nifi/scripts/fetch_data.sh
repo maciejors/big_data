75184a549bc1cf6f4fe8223b6011c4141dda33fc
maciejors <maciej.orslowski@gmail.com>
2023-12-12 16:17:36 +0000

    update fetch_data.sh script

--- nifi/scripts/fetch_data.sh
+++ nifi/scripts/fetch_data.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Please provide a timestamp as an argument."
-    exit 1
+    timestamp=$(date +%s)
+else
+    timestamp=$1
 fi
 
-timestamp=$1
-
 filename="./data/stackoverflow/questions-${timestamp}.json"
-url="https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
-
-curl -k --compressed -X GET $url -o $filename#!/bin/bash
+url="https://api.stackexchange.com/2.3/questions?todate=${timestamp}&order=desc&sort=creation&site=stackoverflow"
+echo $url
+curl -k --compressed -X GET $url -o $filename
 
