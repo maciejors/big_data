cf558b4df9dfb5d1640b1cd0dbe9242c8dc9ab10
Szymon-Gut <szymon.gut01@gmail.com>
2023-12-13 12:25:14 +0000

    add script to load online buffor

---
+++ nifi/scripts/get_essential_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    timestamp=$(date +%s)
+else
+    timestamp=$1
+fi
+
+start_timestamp=$(date -d "January 1, 2023" + %s)
+
+current_timestamp=$(date + %s)
+
+interval = 21600
+
+timestamp=$start_timestamp
+
+while [ $timestamp -lt $current_timestamp ]; do
+    filename="./data/stackoverflow/questions-${timestamp}.json"
+
+    url="https://api.stackexchange.com/2.3/questions?todate=${timestamp}&order=desc&sort=creation&site=stackoverflow"
+    
+    curl -k --compressed -X GET $url -o $filename
+    
+    timestamp=$((timestamp + interval))
