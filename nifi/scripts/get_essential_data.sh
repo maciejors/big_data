0bf4172ca5b11a9bd1d6d8792537f291462bca56
Szymon <72514490+Szymon-Gut@users.noreply.github.com>
2023-12-14 11:13:52 +0100

    Update get_essential_data.sh

--- nifi/scripts/get_essential_data.sh
+++ nifi/scripts/get_essential_data.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-start_timestamp=$(date -d "November 1, 2023" +%s)
-
+start_timestamp=$(date -d "October 13, 2023" +%s)
+end_timestamp=$(date -d "December 13, 2023" +%s)
 current_timestamp=$(date +%s)
 
 interval=21600
 
-timestamp=$start_timestamp
+timestamp=$start_timestamp 
 
-i=0
-while [ $i -lt 300 ]; do
+while [ $timestamp -lt $end_timestamp ]; do
     filename="./data/stackoverflow/questions-${timestamp}.json"
 
     url="https://api.stackexchange.com/2.3/questions?todate=${timestamp}&order=desc&sort=creation&site=stackoverflow"
-    
+
     curl -k --compressed -X GET $url -o $filename
-    
+
     timestamp=$((timestamp + interval))
-    i=$((i + 1))
 done
